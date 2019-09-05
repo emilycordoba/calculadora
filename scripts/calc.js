a9a8085ee86fd433610bbb6e06f67742f67ed09d
Emily <vepicpz@gmail.com>
2019-09-05 08:56:19 -0500

    Finalizado el proyecto

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,12 @@
-function calcular(value){
-    var n1 = document.getElementById('num1').value;
-    var n2 = document.getElementById('num2').value;
-    value === 'sumar' ? alert(`La suma es ${parseInt(n1) + parseInt(n2)}`) :  alert(`La resta es ${parseInt(n1) - parseInt(n2)}`) 
+function calcular(val){
+    var n1 = document.getElementById('num1').value
+    var n2 = document.getElementById('num2').value
+    var result = document.getElementById('result')
+    val == '+' ? result.innerHTML = `${parseInt(n1) + parseInt(n2)}` : val === '-' ? result.innerHTML = `${parseInt(n1) - parseInt(n2)}` : val == '*' ? result.innerHTML = `${parseInt(n1) * parseInt(n2)}` : val === '/' ? result.innerHTML = `${parseInt(n1) / parseInt(n2)}` : alert('Incorrecto')
+    cleanInputs()
 }
+const cleanInputs = () => {
+    document.getElementById('num1').value = ''
+    document.getElementById('num2').value = ''
+}
+const deleteResult = () => result.innerHTML = ''
